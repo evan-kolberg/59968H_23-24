f36c1b724cd78f6e177d2a757a415447b659b509
Evan <ekprogamer13@gmail.com>
2023-10-30 16:14:46 -0400

    pneumatics sys works

--- first/src/main.cpp
+++ first/src/main.cpp
@@ -20,33 +20,30 @@ pros::MotorGroup leftDrive({leftMotor1, leftMotor2, leftMotor3});
 pros::MotorGroup rightDrive({rightMotor1, rightMotor2, rightMotor3});
 
 
-bool leftLLEMUpressed;
-bool centerLLEMUpressed;
-bool rightLLEMUpressed;
-
-
-void checkPneumatics() {
-	while (true) {
-		if (master.get_digital(DIGITAL_B) || leftLLEMUpressed) {
-			solenoid.set_value(0);
-			pros::lcd::print(1, "Pneumatics Triggered");
-			pros::delay(1000);
-		} else {
-			solenoid.set_value(1);
-		}
-
-		pros::delay(1000);
-	}
+void actuatePneu() {
+	solenoid.set_value(true);
+	pros::lcd::print(0, "Actuated");
 }
 
+void retractPneu() {
+	solenoid.set_value(false);
+	pros::lcd::print(0, "Retracted");
+}
+
+void checkController() {
+    if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)) {
+		actuatePneu();
+	}
+	pros::delay(10);
+
+	if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) {
+		retractPneu();
+	}
+	pros::delay(10);
+}
 
 void initialize() {
 	pros::lcd::initialize();
-
-	pros::lcd::register_btn0_cb([]{});
-	pros::lcd::register_btn1_cb([]{});
-	pros::lcd::register_btn2_cb([]{});
-
 }
 
 
@@ -60,21 +57,22 @@ void autonomous() {}
 
 
 void opcontrol() {
+	pros::Task checkControllerThread(checkController);
+
+	pros::lcd::register_btn0_cb(actuatePneu);
+	pros::lcd::register_btn1_cb(retractPneu);
+	pros::lcd::register_btn2_cb([]{});
+
 	while (true) {
 		pros::lcd::print(7, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-
-		// based on the read_button() function, set the LLEMU variables above
-
 
 		int leftStick = master.get_analog(ANALOG_LEFT_Y);
 		int rightStick = master.get_analog(ANALOG_RIGHT_Y);
 
 		leftDrive.move(leftStick);
 		rightDrive.move(rightStick*(-1));
-
-		pros::Task checkPneumaticsThread(checkPneumatics);
 
 		pros::delay(20);
 	}
