e31a06aed31fb3a5a3e467342e1fd4aecb9353ab
Evan <ekprogamer13@gmail.com>
2024-01-04 21:05:08 -0500

    1/4/24 using an inertial sensor for explicit turns, regular odom for movements

--- first/src/main.cpp
+++ first/src/main.cpp
@@ -1,11 +1,14 @@
 #include "main.h"
 #include "okapi/api.hpp"
+#include "MiniPID.h"
 
 using namespace pros;
 
 Controller master(E_CONTROLLER_MASTER);
 
 ADIDigitalOut solenoid('H');
+
+Imu inertial(8);
 
 Motor leftmotor1(1, E_MOTOR_GEARSET_06, true);
 Motor leftmotor2(2, E_MOTOR_GEARSET_06, true);
@@ -25,10 +28,10 @@ void checkController() {
     bool solstate = false;
 
     while (true) {
-        int R = master.get_digital(E_CONTROLLER_DIGITAL_R2) - master.get_digital(E_CONTROLLER_DIGITAL_R1);
+        int R = master.get_digital(E_CONTROLLER_DIGITAL_R1) - master.get_digital(E_CONTROLLER_DIGITAL_R2);
 
         intake.move_velocity(R * 600);
-        cata.move_velocity(master.get_digital(E_CONTROLLER_DIGITAL_L1) * -100);
+        cata.move_velocity(master.get_digital(E_CONTROLLER_DIGITAL_L1) * 100);
 
         if (master.get_digital_new_press(E_CONTROLLER_DIGITAL_B)) {
             solenoid.set_value(solstate = !solstate);
@@ -51,13 +54,26 @@ void disabled() {}
 
 void competition_initialize() {}
 
+MiniPID turnPID = MiniPID(2.0, 0, 0); 
+void intertialTurnToAbsolute(double destination) {
+	while (true) {
+			double sensor_value = inertial.get_rotation();
+			master.clear();
+			master.print(0, 0, "Rotation: %d", (int) sensor_value);
+			double output = turnPID.getOutput(sensor_value, destination);
+			leftdrive.move(output);
+			rightdrive.move(-output);
+
+			delay(20);
+		}
+}
+
 void autonomous() {
     using namespace okapi;
 
 	leftdrive.set_brake_modes(E_MOTOR_BRAKE_COAST);
 	rightdrive.set_brake_modes(E_MOTOR_BRAKE_COAST);
-	cata.set_brake_mode(E_MOTOR_BRAKE_COAST);
-	intake.set_brake_mode(E_MOTOR_BRAKE_COAST);
+	intake.set_brake_mode(E_MOTOR_BRAKE_HOLD);
 
     auto chassis = ChassisControllerBuilder()
         .withMotors({1, 2, 3}, {11, 12, 13}) // 1, 2, 3 are alr reversed b/c of motor constructors
@@ -65,8 +81,8 @@ void autonomous() {
     	.withDimensions({AbstractMotor::gearset::blue, (60 / 36)}, {{3.25_in, 13_in}, imev5BlueTPR})
 		// {P, I, D}
 		.withGains(
-			{0.005, 0, 0.0005}, // distance control
-			{0.001, 0, 0.0001}  // turn control
+			{0, 0, 0}, // distance control
+			{0, 0, 0}  // turn control ~ using an inertial sensor instead, don't need this. It's for the turnAngle funciton.
 		)
         .build();
 
@@ -77,19 +93,25 @@ void autonomous() {
 
 	// {x_unit, y_unit, headingθ_unit}
     profileController->generatePath({ // positive x is forward, positive y is to the right, robot starts at 0 on unit circle
+									  // if you want to go in reverse, use a true paremeter in the setTarget() func
 		{0_m, 0_m, 0_deg},
 		{1_m, 1_m, 0_deg},
-
     }, "auton");
 
 	/* 
     profileController->setTarget("auton");
 	profileController->waitUntilSettled();
 	*/
-	
-	chassis->setMaxVelocity(100);
+
+
+
+	chassis->setMaxVelocity(150);
 	chassis->moveDistance(1_m);
-	chassis->turnAngle(90_deg*2);
+	intertialTurnToAbsolute(90);
+
+
+
+
 
 }
 
