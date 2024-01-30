06ea21a6f4900e4ddf5e18920ab8f8d1d1d3a1a0
Evan <ekprogamer13@gmail.com>
2024-01-29 23:00:50 -0500

    1/29/24 transferred to ez template 3, installed lemlib

--- second_EZ-Template-Example-Proj-2.2.0/src/main.cpp
+++ second_EZ-Template-Example-Proj-2.2.0/src/main.cpp
@@ -26,7 +26,7 @@ Drive chassis (
   //    (or gear ratio of tracking wheel)
   // eg. if your drive is 84:36 where the 36t is powered, your RATIO would be 2.333.
   // eg. if your drive is 36:60 where the 60t is powered, your RATIO would be 0.6.
-  ,(5/3)
+  ,(60. / 36.)
 
 
   // Uncomment if using tracking wheels
